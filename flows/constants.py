abb4dcc4b949e6fe12464b58974deefd9c6fa71d
biwano <biwano@gmail.com>
2023-05-30 14:48:36 +0200

    Adds raw ethereum offsets

--- flows/constants.py
+++ flows/constants.py
@@ -1,3 +1,12 @@
 CARBON_SUBGRAPH_URL = (
     "https://api.thegraph.com/subgraphs/name/klimadao/polygon-bridged-carbon"
 )
+CARBON_MOSS_ETH_SUBGRAPH_URL = (
+    "https://api.thegraph.com/subgraphs/name/originalpkbims/ethcarbonsubgraph"
+)
+CARBON_ETH_SUBGRAPH_URL = (
+    "https://api.thegraph.com/subgraphs/name/klimadao/ethereum-bridged-carbon"
+)
+CARBON_MOSS_ETH_TEST_SUBGRAPH_URL = (
+    "https://api.thegraph.com/subgraphs/name/originalpkbims/ethcarbonsubgraphtest"
+)
