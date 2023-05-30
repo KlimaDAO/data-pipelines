dc44725e2a8eb59dc33a448c790226c7c3d8b958
biwano <biwano@gmail.com>
2023-05-30 15:34:59 +0200

    Adds raw klima retirements flows

---
+++ flows/raw_polygon_klima_retirements.py
@@ -0,0 +1,75 @@
+""" Raw Polygon Klima retirements flow """
+from prefect import flow, task
+from subgrounds.subgrounds import Subgrounds
+from subgrounds.subgraph import SyntheticField
+import utils
+import constants
+
+
+SLUG = "raw_polygon_klima_retirements"
+
+
+@task()
+def fetch_polygon_klima_retirements_task():
+    """Fetches Polygon Klima retirements"""
+
+    sg = Subgrounds()
+    carbon_data = sg.load_subgraph(constants.CARBON_SUBGRAPH_URL)
+
+    carbon_data.KlimaRetire.datetime = SyntheticField(
+        utils.format_timestamp,
+        SyntheticField.STRING,
+        carbon_data.KlimaRetire.timestamp,
+    )
+
+    carbon_data.KlimaRetire.proof = SyntheticField(
+        lambda tx_id: f'https://polygonscan.com/tx/{tx_id}',
+        SyntheticField.STRING,
+        carbon_data.KlimaRetire.transaction.id,
+    )
+
+    klima_retirees = carbon_data.Query.klimaRetires(
+        orderBy=carbon_data.KlimaRetire.timestamp,
+        orderDirection="desc",
+        first=utils.get_max_records()
+    )
+
+    return sg.query_df(
+        [
+            klima_retirees.beneficiaryAddress,
+            klima_retirees.offset.projectID,
+            klima_retirees.offset.bridge,
+            klima_retirees.token,
+            klima_retirees.datetime,
+            klima_retirees.amount,
+            klima_retirees.proof
+        ]
+    )
+
+
+@task()
+def validate_polygon_klima_retirements_task(df):
+    """Validates Polygon Klima retirements"""
+    utils.validate_against_latest_dataframe(SLUG, df)
+
+
+@flow()
+def raw_polygon_klima_retirements():
+    """Fetches Polygon Klima retirements and stores it"""
+    utils.raw_data_flow(
+        slug=SLUG,
+        fetch_data_task=fetch_polygon_klima_retirements_task,
+        validate_data_task=validate_polygon_klima_retirements_task,
+    )
+
+
+@flow()
+def raw_polygon_klima_retirements_flow(result_storage):
+    """Fetches Polygon Klima retirements and stores it"""
+    raw_polygon_klima_retirements.with_options(result_storage=result_storage)()
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    raw_polygon_klima_retirements()
