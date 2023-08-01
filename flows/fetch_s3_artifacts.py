1423102329011599136d50d455bc079ade27e783
biwano <biwano@gmail.com>
2023-08-01 15:09:52 +0200

    Remade fetch_S3_artifacts => fetch_s3_artifacts

--- flows/fetch_s3_artifacts.py
+++ flows/fetch_s3_artifacts.py
@@ -4,7 +4,7 @@ from prefect.logging import get_run_logger
 import utils
 
 
-def fetch_S3_artifact(path):
+def fetch_s3_artifact(path):
     logger = get_run_logger()
     logger.info(f"fetching {path}")
     s3 = utils.get_s3()
@@ -16,16 +16,16 @@ def fetch_S3_artifact(path):
 
 
 @utils.flow_with_result_storage
-def fetch_S3_artifacts(result_storage=None):
+def fetch_s3_artifacts(result_storage=None):
     """Deletes old artifacts (exept those suffixed with latest)"""
     s3 = utils.get_s3()
     for f in s3.ls(utils.get_s3_path("lake"), detail=True):
         key = f.get("Key")
         if key.endswith("-latest"):
-            fetch_S3_artifact(key)
+            fetch_s3_artifact(key)
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
     load_dotenv()
-    fetch_S3_artifacts()
+    fetch_s3_artifacts()
