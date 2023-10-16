3d9c707d63200a930b644d0ed45404ddddb67124
biwano <biwano@gmail.com>
2023-10-16 16:46:13 +0200

    Revert "Run subflows in subprocesses"
    This reverts commit b2c0705c95e98e1ddaa883c10b1c8a219a8d3048.

--- flows/build_all.py
+++ flows/build_all.py
@@ -1,65 +1,112 @@
 """ Raw Verra data flow """
 from utils import run, flow_with_result_storage
-import os
+from raw_verra_data import raw_verra_data_flow
+
+from verra_data import verra_data_flow
+from verra_data_v2 import verra_data_v2_flow
+from verra_retirements import verra_retirements_flow
+
+from raw_polygon_retired_offsets import raw_polygon_retired_offsets_flow
+from raw_polygon_pools_retired_offsets import raw_polygon_pools_retired_offsets_flow
+from raw_polygon_pools_redeemed_offsets import raw_polygon_pools_redeemed_offsets_flow
+from raw_polygon_pools_deposited_offsets import raw_polygon_pools_deposited_offsets_flow
+from raw_polygon_klima_retirements import raw_polygon_klima_retirements_flow
+from raw_polygon_klima_retirements_daily import raw_polygon_klima_retirements_daily_flow
+from raw_polygon_bridged_offsets import raw_polygon_bridged_offsets_flow
+from raw_offsets_holders_data import raw_offsets_holders_data_flow
+from raw_eth_retired_offsets import raw_eth_retired_offsets_flow
+from raw_eth_moss_retired_offsets import raw_eth_moss_retired_offsets_flow
+from raw_eth_moss_bridged_offsets import raw_eth_moss_bridged_offsets_flow
+from raw_eth_bridged_offsets_transactions import raw_eth_bridged_offsets_transactions_flow
+
+from raw_eth_carbon_metrics import raw_eth_carbon_metrics_flow
+from raw_celo_carbon_metrics import raw_celo_carbon_metrics_flow
+from raw_polygon_carbon_metrics import raw_polygon_carbon_metrics_flow
+from eth_carbon_metrics import eth_carbon_metrics_flow
+from celo_carbon_metrics import celo_carbon_metrics_flow
+from polygon_carbon_metrics import polygon_carbon_metrics_flow
+
+from raw_assets_prices import raw_assets_prices_flow
+from current_assets_prices import current_assets_prices_flow
+from assets_prices import assets_prices_flow
+
+
+from polygon_retired_offsets import polygon_retired_offsets_flow
+from polygon_retired_offsets_v2 import polygon_retired_offsets_v2_flow
+from polygon_bridged_offsets import polygon_bridged_offsets_flow
+from polygon_bridged_offsets_v2 import polygon_bridged_offsets_v2_flow
+from polygon_pools_retired_offsets import polygon_pools_retired_offsets_flow
+from polygon_pools_redeemed_offsets import polygon_pools_redeemed_offsets_flow
+from polygon_pools_deposited_offsets import polygon_pools_deposited_offsets_flow
+from eth_moss_bridged_offsets import eth_moss_bridged_offsets_flow
+from eth_moss_bridged_offsets_v2 import eth_moss_bridged_offsets_v2_flow
+from eth_retired_offsets import eth_retired_offsets_flow
+from eth_retired_offsets_v2 import eth_retired_offsets_v2_flow
+
+from polygon_klima_retirements import polygon_klima_retirements_flow
+from polygon_klima_retirements_daily import polygon_klima_retirements_daily_flow
+from all_retirements import all_retirements_flow
+
+from offsets_holders_data import offsets_holders_data_flow
+
+from tokens_data import tokens_data_flow
+from tokens_data_v2 import tokens_data_v2_flow
 
 
 @flow_with_result_storage
 def build_all_flow(result_storage=None):
     """Run all the build flows"""
-    # set up environement for subflows
-    if result_storage:
-        os.environ["DATA_PIPELINES_RESULT_STORAGE"] = result_storage
+    """
+    run(raw_verra_data_flow)
+    run(raw_polygon_retired_offsets_flow)
+    run(raw_polygon_pools_retired_offsets_flow)
+    run(raw_polygon_pools_redeemed_offsets_flow)
+    run(raw_polygon_pools_deposited_offsets_flow)
+    run(raw_polygon_klima_retirements_flow)
+    run(raw_polygon_klima_retirements_daily_flow)
+    run(raw_polygon_bridged_offsets_flow)
+    run(raw_offsets_holders_data_flow)
+    run(raw_eth_retired_offsets_flow)
+    run(raw_eth_moss_retired_offsets_flow)
+    run(raw_eth_moss_bridged_offsets_flow)
+    run(raw_eth_bridged_offsets_transactions_flow)
+    run(raw_eth_carbon_metrics_flow)
+    run(raw_celo_carbon_metrics_flow)
+    run(raw_polygon_carbon_metrics_flow)
+    run(raw_assets_prices_flow)
+    """
+    run(current_assets_prices_flow)
+    run(assets_prices_flow)
 
-    # run subflows
-    run("raw_verra_data")
-    run("raw_polygon_retired_offsets")
-    run("raw_polygon_pools_retired_offsets")
-    run("raw_polygon_pools_redeemed_offsets")
-    run("raw_polygon_pools_deposited_offsets")
-    run("raw_polygon_klima_retirements")
-    run("raw_polygon_klima_retirements_daily")
-    run("raw_polygon_bridged_offsets")
-    run("raw_offsets_holders_data")
-    run("raw_eth_retired_offsets")
-    run("raw_eth_moss_retired_offsets")
-    run("raw_eth_moss_bridged_offsets")
-    run("raw_eth_bridged_offsets_transactions")
-    run("raw_eth_carbon_metrics")
-    run("raw_celo_carbon_metrics")
-    run("raw_polygon_carbon_metrics")
-    run("raw_assets_prices")
+    run(verra_data_flow)
+    run(polygon_retired_offsets_flow)
+    run(polygon_bridged_offsets_flow)
+    run(polygon_pools_retired_offsets_flow)
+    run(polygon_pools_redeemed_offsets_flow)
+    run(polygon_pools_deposited_offsets_flow)
 
-    run("current_assets_prices")
-    run("assets_prices")
-    run("verra_data")
-    run("polygon_retired_offsets")
-    run("polygon_bridged_offsets")
-    run("polygon_pools_retired_offsets")
-    run("polygon_pools_redeemed_offsets")
-    run("polygon_pools_deposited_offsets")
+    run(eth_moss_bridged_offsets_flow)
+    run(eth_retired_offsets_flow)
 
-    run("eth_moss_bridged_offsets")
-    run("eth_retired_offsets")
+    run(verra_data_v2_flow)
+    run(verra_retirements_flow)
+    run(polygon_retired_offsets_v2_flow)
+    run(polygon_bridged_offsets_v2_flow)
+    run(eth_moss_bridged_offsets_v2_flow)
+    run(eth_retired_offsets_v2_flow)
 
-    run("verra_data_v2")
-    run("verra_retirements")
-    run("polygon_retired_offsets_v2")
-    run("polygon_bridged_offsets_v2")
-    run("eth_moss_bridged_offsets_v2")
-    run("eth_retired_offsets_v2")
+    run(tokens_data_flow)
+    run(offsets_holders_data_flow)
 
-    run("tokens_data")
-    run("offsets_holders_data")
+    run(polygon_klima_retirements_flow)
+    run(polygon_klima_retirements_daily_flow)
+    run(all_retirements_flow)
 
-    run("polygon_klima_retirements")
-    run("polygon_klima_retirements_daily")
-    run("all_retirements")
+    run(eth_carbon_metrics_flow)
+    run(celo_carbon_metrics_flow)
+    run(polygon_carbon_metrics_flow)
 
-    run("eth_carbon_metrics")
-    run("celo_carbon_metrics")
-    run("polygon_carbon_metrics")
-
-    run("tokens_data_v2")
+    run(tokens_data_v2_flow)
 
 
 if __name__ == "__main__":
--- flows/utils.py
+++ flows/utils.py
@@ -4,7 +4,6 @@ import os
 import s3fs
 import base64
 import json
-import subprocess
 from datetime import datetime
 from prefect.context import FlowRunContext
 from prefect.logging import get_run_logger
@@ -200,13 +199,11 @@ def raw_data_flow(slug, fetch_data_task, validate_data_task, historize=True):
 
 def run(flow):
     """Runs a flow and log errors"""
-    cwd = os.path.dirname(os.path.abspath(__file__))
-    proc = subprocess.Popen(["python", f"{flow}.py"], shell=False, cwd=cwd)
-    proc.communicate()
-
-    if proc.returncode:
+    state = flow(return_state=True)
+    maybe_result = state.result(raise_on_failure=False)
+    if isinstance(maybe_result, ValueError):
         logger = get_run_logger()
-        logger.warn(f"flow {flow} failed")
+        logger.warn(f"flow {flow.__name__} failed")
 
 
 def task_with_backoff(func):
