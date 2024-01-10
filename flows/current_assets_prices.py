3174c15fbe7c51519ca451b48e0ec2430d9099f4
biwano <biwano@gmail.com>
2024-01-10 16:11:49 +0100

    Use Klimadao pairs subgraph to compute current prices

--- flows/current_assets_prices.py
+++ flows/current_assets_prices.py
@@ -2,72 +2,49 @@
 from prefect import task
 import utils
 import constants
-from pycoingecko import CoinGeckoAPI
 import pandas as pd
 import time
+from subgrounds.subgrounds import Subgrounds
 
 
 SLUG = "current_assets_prices"
 
 
-def uni_v2_pool_price(web3, pool_address, decimals, base_price=1):
-    """
-    Calculate the price of a SushiSwap liquidity pool, using the provided
-    pool address, decimals of the first token, and multiplied by
-    base_price if provided for computing multiple pool hops.
-    """
-    uni_v2_abi = utils.load_abi("uni_v2_pool.json")
-    pool_contract = web3.eth.contract(address=pool_address, abi=uni_v2_abi)
-
-    reserves = pool_contract.functions.getReserves().call()
-    token_price = reserves[0] * base_price * 10**decimals / reserves[1]
-
-    return token_price
-
-
-def klima_usdc_price(web3):
-    return uni_v2_pool_price(web3, constants.KLIMA_USDC_ADDRESS, constants.USDC_DECIMALS - constants.KLIMA_DECIMALS)
+def get_pair_price(df, pair_address):
+    return df[df["pairs_id"] == pair_address.lower()].iloc[0]["pairs_currentprice"]
 
 
 @task()
 def fetch_current_assets_prices_task():
     """Fetches latest asset prices"""
-    cg = CoinGeckoAPI()
+    sg = Subgrounds()
     tokens_dict = constants.TOKENS
-    current_price_only_token_list = ["UBO", "NBO"]
     df = pd.DataFrame()
-    web3 = utils.get_polygon_web3()
+
+    # Fetch data
+    pairs_sg = sg.load_subgraph(constants.PAIRS_SUBGRAPH_URL)
+    pairs = pairs_sg.Query.pairs()
+    df = sg.query_df([
+        pairs.id,
+        pairs.token0.symbol,
+        pairs.token1.symbol,
+        pairs.currentprice
+    ])
+
+    # Compute prices
     prices = {
         "Date": time.time()
     }
+    klima_price = get_pair_price(df, constants.KLIMA_USDC_ADDRESS)
     for i in tokens_dict.keys():
-        if i not in current_price_only_token_list:
-            data = cg.get_price(ids=tokens_dict[i]["cg_id"],
-                                vs_currencies="usd"
-                                )
-            price = data[tokens_dict[i]["cg_id"]]["usd"]
-            prices[f"{i}_Price"] = price
-    for i in current_price_only_token_list:
-        if i == "UBO":
-            klima_price = klima_usdc_price(web3)
-            token_price = uni_v2_pool_price(
-                web3,
-                web3.to_checksum_address(tokens_dict[i]["Pair Address"]),
-                constants.KLIMA_DECIMALS - tokens_dict[i]["Decimals"],
-            )
-            price = klima_price / token_price
-        elif i == "NBO":
-            klima_price = klima_usdc_price(web3)
-            token_price = uni_v2_pool_price(
-                web3,
-                web3.to_checksum_address(tokens_dict[i]["Pair Address"]),
-                constants.KLIMA_DECIMALS,
-            )
-            price = token_price * klima_price
-
+        price = get_pair_price(df, tokens_dict[i]["Pair Address"])
+        if tokens_dict[i]["Is Klima Pair"]:
+            price *= klima_price
         prices[f"{i}_Price"] = price
 
     df = pd.DataFrame(prices, index=[0])
+
+    # Add date
     df["Date"] = (
         pd.to_datetime(df["Date"], unit="s")
         .dt.tz_localize("UTC")
